2312589dbaa8ea044ebf5aed62928b9530093f5a
Sullivan <sullivan.david@epitech.eu>
2018-05-01 14:37:12 +0100

    Add 2 core external libraries for Jackson

--- WebCrawler/src/Parser/Parser_URL_Input.java
+++ WebCrawler/src/Parser/Parser_URL_Input.java
@@ -1,11 +1,19 @@
 package Parser;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 
 
 public class Parser_URL_Input {
+    @SuppressWarnings("unchecked")
+
     public static void file_input() {
-        
+        File json_file = new File("../Websites.json");
     }
+
+
 }
