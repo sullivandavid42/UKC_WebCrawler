a871774528d0a179ffa972b4586845817f3f008d
Sullivan <sullivan.david@epitech.eu>
2018-05-01 15:30:05 +0100

    JsonSimple starting

--- WebCrawler/src/com/company/Main.java
+++ WebCrawler/src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import Parser.Parser_URL_Input;
+
 public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("test");
+        Parser.Parser_URL_Input tt = new Parser_URL_Input();
+
+        try {
+        tt.file_input(); } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
