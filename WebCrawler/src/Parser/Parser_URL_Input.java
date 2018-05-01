f271a25574364dfb33043e8f6d30f07a97fe8752
Sullivan <sullivan.david@epitech.eu>
2018-05-01 15:54:43 +0100

    Add URL list to List<String>

--- WebCrawler/src/Parser/Parser_URL_Input.java
+++ WebCrawler/src/Parser/Parser_URL_Input.java
@@ -10,24 +10,27 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Parser_URL_Input
 {
     @SuppressWarnings("unchecked")
 
-    public void file_input() throws Exception {
+    public List<String> file_input() throws Exception {
         JSONParser parser = new JSONParser();
         JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("C:\\Users\\Sullivan\\IdeaProjects\\UKC_WebCrawler\\WebCrawler\\Websites.json"));
         JSONArray ja = (JSONArray) jsonObject.get("routes");
-
+        List<String> list_url = new ArrayList<>();
         for(int i=0;i<ja.size() ; i++){
             JSONObject tempJsonObj = (JSONObject) ja.get(i);
+            list_url.add(tempJsonObj.get("url").toString());
             System.out.println(String.format("ID: %s", tempJsonObj.get("id").toString()));
             System.out.println(String.format("URL: %s", tempJsonObj.get("url").toString()));
             System.out.println(String.format("Flag-Home: %s", tempJsonObj.get("home")));
         }
 
-
+        return list_url;
     }
 
 
