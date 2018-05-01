aec927e0979983ed7fa2cb3204ea054320cef76f
sullivan <sullivan.david@epitech.eu>
2018-05-01 14:46:45 +0100

    Usage of JSONSimple

--- WebCrawler/src/Parser/Parser_URL_Input.java
+++ WebCrawler/src/Parser/Parser_URL_Input.java
@@ -1,18 +1,16 @@
 package Parser;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
+
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 
-
-public class Parser_URL_Input {
+public class Parser_URL_Input
+{
     @SuppressWarnings("unchecked")
 
-    public static void file_input() {
-        File json_file = new File("../Websites.json");
+    public static void file_input() throws Exception {
+
+
     }
 
 
