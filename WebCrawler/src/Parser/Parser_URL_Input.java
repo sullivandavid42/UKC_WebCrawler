3ddcb6c977f71048a8342e9479c6b6f3169ddaaf
sullivan <sullivan.david@epitech.eu>
2018-05-01 15:51:33 +0100

    push parser url

--- WebCrawler/src/Parser/Parser_URL_Input.java
+++ WebCrawler/src/Parser/Parser_URL_Input.java
@@ -6,27 +6,25 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.sql.SQLOutput;
 
 public class Parser_URL_Input
 {
     @SuppressWarnings("unchecked")
 
-    public void file_input(){
+    public void file_input() throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("C:\\Users\\Sullivan\\IdeaProjects\\UKC_WebCrawler\\WebCrawler\\Websites.json"));
+        JSONArray ja = (JSONArray) jsonObject.get("routes");
 
-        try {
-            JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new FileReader("C:\\Users\\Sullivan\\IdeaProjects\\UKC_WebCrawler\\WebCrawler\\Websites.json"));
-            JSONObject jsonobj = (JSONObject)obj;
-
-
-            String urltest = (String) jsonobj.get("url");
-            System.out.println(urltest);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        for(int i=0;i<ja.size() ; i++){
+            JSONObject tempJsonObj = (JSONObject) ja.get(i);
+            System.out.println(String.format("ID: %s", tempJsonObj.get("id").toString()));
+            System.out.println(String.format("URL: %s", tempJsonObj.get("url").toString()));
+            System.out.println(String.format("Flag-Home: %s", tempJsonObj.get("home")));
         }
 
 
