92fd223a9764fd42d88ec4454f3d3dedc1978e8b
sullivan <sullivan.david@epitech.eu>
2018-05-01 15:30:05 +0100

    JsonSimple starting

--- WebCrawler/src/Parser/Parser_URL_Input.java
+++ WebCrawler/src/Parser/Parser_URL_Input.java
@@ -1,14 +1,33 @@
 package Parser;
 
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Parser_URL_Input
 {
     @SuppressWarnings("unchecked")
 
-    public static void file_input() throws Exception {
+    public void file_input(){
+
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("C:\\Users\\Sullivan\\IdeaProjects\\UKC_WebCrawler\\WebCrawler\\Websites.json"));
+            JSONObject jsonobj = (JSONObject)obj;
+
+
+            String urltest = (String) jsonobj.get("url");
+            System.out.println(urltest);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
