909e8d344db169613f87b461bc10e7fb0d857264
sullivan <sullivan.david@epitech.eu>
2018-05-28 10:49:32 +0100

    DOM_Class

---
+++ WebCrawler/src/Dom_Interactions/Dom_Opening.java
@@ -0,0 +1,18 @@
+package Dom_Interactions;
+
+import Parser.Parser_URL_Input;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.List;
+
+public class Dom_Opening {
+
+       public void Get_DOM() throws Exception {
+           List<String> _url_list = new Parser_URL_Input().file_input();
+
+       }
+
+}
