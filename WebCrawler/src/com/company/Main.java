bfa54c16b590d69468f924c836e25fbb5c4b638b
sullivan <sullivan.david@epitech.eu>
2018-06-12 10:54:31 +0100

    -Remove all warnings -Modification event order in main (Dom class instanciate ParserURL now) -Remove debug

--- WebCrawler/src/com/company/Main.java
+++ WebCrawler/src/com/company/Main.java
@@ -6,17 +6,10 @@ import Parser.Parser_URL_Input;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("test");
-       // Parser.Parser_URL_Input tt = new Parser_URL_Input();
         Dom_Opening dom_reader = new Dom_Opening();
         try {
         dom_reader.Get_DOM(); } catch (Exception e) {
             e.printStackTrace();
         }
-
-       /* try {
-        tt.file_input(); } catch (Exception e) {
-            e.printStackTrace();
-        } */
     }
 }
