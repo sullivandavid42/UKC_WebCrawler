b0fce48910254f08043a603aca92c7d52b3006f6
Sullivan <sullivan.david@epitech.eu>
2018-06-12 11:32:24 +0100

    - Add bug gesture (try to fetch each URL and catch error.) Valide URLs go to new List<string>

--- WebCrawler/src/Dom_Interactions/Dom_Opening.java
+++ WebCrawler/src/Dom_Interactions/Dom_Opening.java
@@ -6,18 +6,43 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Dom_Opening {
 
+
+        /*
+        This method instanciate Parser_URL_Input() class and execute file_input.
+        Each website in the JSON file is trying to be ping : If ping OK -> DOM reader
+                                                             If not -> Remove URL from JSON file
+         */
        public void Get_DOM() throws Exception {
            int _size_urllist;
            List<String> _url_list = new Parser_URL_Input().file_input();
+           List<String> _verified_list = new ArrayList<String>();
+
            _size_urllist = _url_list.size();
            for (int i = 0; i < _size_urllist; i++) {
-               Document _doc = Jsoup.connect("https://www." + _url_list.get(i)).get();
-               System.out.println(_doc.title());
+               try {
+                   Document _doc = Jsoup.connect("https://www." + _url_list.get(i)).get();
+                   System.out.println(_doc.title());
+                   _verified_list.add("https://www." + _url_list.get(i));
+
+               } catch (UnknownHostException e) {
+                   System.err.println("Unknow Host. URL is not reacheable");
+                   e.printStackTrace();
+               }
            }
+       }
+
+       /*
+       This method aim to find the login URK from each website in the JSON file.
+       When login page has been found, the URL has to be save in a List<String>
+        */
+       public void Find_LoginURLs() {
+
        }
 
 }
