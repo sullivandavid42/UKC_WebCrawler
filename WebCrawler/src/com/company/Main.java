3c7bc77d7d2ab9118adc2d2ee2c59d4432b100d4
sullivan <sullivan.david@epitech.eu>
2018-06-12 10:41:47 +0100

    add DOM first gesture - No error control if URL does not exist yet)

--- WebCrawler/src/com/company/Main.java
+++ WebCrawler/src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import Dom_Interactions.Dom_Opening;
 import Parser.Parser_URL_Input;
 
 
@@ -6,11 +7,16 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("test");
-        Parser.Parser_URL_Input tt = new Parser_URL_Input();
-
+       // Parser.Parser_URL_Input tt = new Parser_URL_Input();
+        Dom_Opening dom_reader = new Dom_Opening();
         try {
-        tt.file_input(); } catch (Exception e) {
+        dom_reader.Get_DOM(); } catch (Exception e) {
             e.printStackTrace();
         }
+
+       /* try {
+        tt.file_input(); } catch (Exception e) {
+            e.printStackTrace();
+        } */
     }
 }
